82bf3cefd491f8e0887b5bb8138238e549e57bef
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-08-11 22:08:13 +0100

    create login controller and exception handler

--- src/main/java/com/leandroucuamba/Angolib/controller/UsuarioLoginController.java
+++ src/main/java/com/leandroucuamba/Angolib/controller/UsuarioLoginController.java
@@ -1,5 +1,34 @@
 package com.leandroucuamba.Angolib.controller;
 
+import com.leandroucuamba.Angolib.dto.TokenDoUsuario;
+import com.leandroucuamba.Angolib.dto.UsuarioDeLogin;
+import com.leandroucuamba.Angolib.dto.UsuarioDeRegistro;
+import com.leandroucuamba.Angolib.entity.Usuario;
+import com.leandroucuamba.Angolib.service.JWTService;
+import com.leandroucuamba.Angolib.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api")
 public class UsuarioLoginController {
-    
+
+    private UsuarioService usuarioService;
+
+    private JWTService jwtService;
+
+    @PostMapping("/registro")
+    public ResponseEntity<Usuario> cadastroUsuario(@RequestBody UsuarioDeRegistro usuarioDeRegistro){
+        var usuarioParaSalvar = usuarioService.cadastraUsuario(usuarioDeRegistro);
+        return ResponseEntity.ok(usuarioParaSalvar);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDoUsuario> autenticaUsuario(@RequestBody UsuarioDeLogin usuarioDeLogin){
+        var tokenDoUsuario = jwtService.autenticaUsuario(usuarioDeLogin);
+        return ResponseEntity.ok(tokenDoUsuario);
+    }
 }
--- src/main/java/com/leandroucuamba/Angolib/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/com/leandroucuamba/Angolib/controller/exception/ControllerExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.leandroucuamba.Angolib.controller.exception;
 
+import com.leandroucuamba.Angolib.service.exception.LoginInvalidoException;
+import com.leandroucuamba.Angolib.service.exception.UsuarioJaExisteException;
+import com.leandroucuamba.Angolib.service.exception.UsuarioNaoExisteException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,46 @@ public class ControllerExceptionHandler {
     public ResponseEntity<ErrorResponse> notFound(EntityNotFound e, HttpServletRequest request){
         String error = "Not Found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(UsuarioJaExisteException.class)
+    public ResponseEntity<ErrorResponse> usuarioJaExisteError(UsuarioJaExisteException e, HttpServletRequest request){
+        String error = "Usuário já está cadastrado no sistema!";
+        HttpStatus status = HttpStatus.CONFLICT;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(UsuarioNaoExisteException.class)
+    public ResponseEntity<ErrorResponse> usuarioNaoExisteError(UsuarioNaoExisteException e, HttpServletRequest request){
+        String error = "Usuário não existe no sistema!";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(LoginInvalidoException.class)
+    public ResponseEntity<ErrorResponse> LoginInvalidoError(LoginInvalidoException e, HttpServletRequest request){
+        String error = "Login Inválido. Tente novamente...";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(UsuarioNaoExisteException.class)
+    public ResponseEntity<ErrorResponse> UsuarioNaoExisteError(UsuarioNaoExisteException e, HttpServletRequest request){
+        String error = "Usuário não tem permissão";
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<ErrorResponse> ErroDeSegurancaDoToken(SecurityException e, HttpServletRequest request){
+        String error = "Token foi inválido!";
+        HttpStatus status = HttpStatus.FORBIDDEN;
         ErrorResponse err = new ErrorResponse(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
